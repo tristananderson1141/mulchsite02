96466c7e1f97c864315ea218b167fcbd6be10aca
tristananderson1141 <muttonjones72@outlook.com>
2021-03-10 10:28:33 -0500

    copying from previous mulchsite to mulchsite02

---
+++ README.md
@@ -0,0 +1,11 @@
+# mulchsite
+a website based on html5 and javascript using bulma css styling. the website is to display information about a mulch and topsoil business and allow interested potential customers to reach out to the business owner.
+
+this site is going to have minimal interactivity and mostly is going to provide information to the potential customer about:
+
+1. products Lawn Life has to offer
+2. pricing and availability of the products
+3. location of the main location
+4. contact information
+5. an interactive tool for submitting to the business a request to do business which includes their contact information, and the products and quantities they think
+  they will be needing.
---
+++ processRequest.js
@@ -0,0 +1,84 @@
+// Following 3 functions are used to navigate different HTML pages for the mulchsite
+function openCalcPage() {
+    window.open("Mulch-Area-Calculator.html", "_self");
+}
+
+function openHomePage() {
+    window.open("index.html","_self");
+}
+
+function openAboutUs() {
+    window.open("About-Us.html", "_self");
+}
+
+
+// Following 3 functions are used to auto scroll to different categories of products
+function mulchLocater() {
+
+    location.href = "#";
+    location.href = "index.html#mulchHeader";        
+}
+
+function soilLocater() {
+    
+    location.href = "#";
+    location.href = "index.html#soilHeader";
+}
+
+function sandLocater() {
+    
+    location.href = "#";
+    location.href = "index.html#sandHeader";
+}
+
+// Function is used to calculate quantity of mulch required for a given area
+function calcArea() {
+
+    var length = document.getElementById("length").value;
+    var width = document.getElementById("width").value;
+    var depth = document.getElementById("depth").value;
+    depth = depth / 12;
+
+    var area = length * width * depth;
+
+    area = area / 3;
+    area = area.toFixed(2);
+
+    var result = area + " cubic yards"
+
+    document.getElementById("result").textContent = result;
+}
+
+// Event listener for navigation options
+document.getElementById("mulchCalc").addEventListener("click", openCalcPage);
+document.getElementById("homePage").addEventListener("click", openHomePage);
+document.getElementById("aboutUs").addEventListener("click", openAboutUs);
+
+document.getElementById("locateMulch").addEventListener("click", mulchLocater);
+document.getElementById("locateSoil").addEventListener("click", soilLocater);
+document.getElementById("locateSand").addEventListener("click", sandLocater);
+
+// Allows burger to display header items when active
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elemetns
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar-burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(e1 => {
+            e1.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = e1.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                e1.classList.toggle("is-active");
+                $target.classList.toggle('is-active');
+            })
+        })
+    }
+});
